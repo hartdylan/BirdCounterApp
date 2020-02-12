5aed992b1c72268f31e86d83875e188924df5178
Dylan Hart <dylanhart16@augustana.edu>
2020-02-11 18:59:52 -0600

    Started to implement Firebase to store data. Not successful...yet.

--- app/src/main/java/augustana/birdcounter/MainActivity.java
+++ app/src/main/java/augustana/birdcounter/MainActivity.java
@@ -1,8 +1,10 @@
 package augustana.birdcounter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -12,29 +14,40 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
-import java.util.HashMap;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.w3c.dom.Document;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    String[] birds = {"Blue Jay", "Kiwi"};
+    String[] birds = {"Blue Jay", "Kiwi", "Bird3", "Bird4","Bird5", "Bird6", "Bird7", "Bird8", "Bird9", "Bird10", "Bird11", "Bird12", "Bird13", "Bird14","Bird15", "Bird16", "Bird17", "Bird18", "Bird19", "Bird20"};
     HashMap<String, Integer> mp;
     String curBird;
-    int totalFound;
     Spinner sp;
     Adapter ad;
     TextView name,count;
     Button fBtn, uBtn;
     ImageView bImg;
+    FirebaseDatabase db;
+    DatabaseReference ref;
+    FirebaseFirestore fStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mp = initializeMap(birds);
-
         bImg = findViewById(R.id.birdImg);
         name = findViewById(R.id.birdName);
         count = findViewById(R.id.foundText);
@@ -56,19 +69,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else if (curBird.equals("Kiwi")) {
                     bImg.setImageResource(R.drawable.kiwi);
                 }
-
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
     }
 
-    public void onClick(View view, TextView textView) {
-
-    }
 
     @Override
     public void onClick(View v) {
@@ -77,7 +84,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (v == uBtn && mp.get(curBird) > 0) {
             mp.put(curBird, mp.get(curBird)-1);
         }
-         updateDisplayCount();
+        updateDisplayCount();
+//        updateDB();
     }
 
     public HashMap<String, Integer> initializeMap(String[] birdTypes) {
@@ -85,12 +93,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for(String name: birdTypes) {
             map.put(name, 0);
         }
-        System.out.println(map.get("Blue Jay"));
-        System.out.println(map.get("Kiwi"));
         return map;
     }
 
     public void updateDisplayCount() {
         count.setText("Found : " + mp.get(curBird));
+        }
+
+//    public void updateDB() {
+//        fStore = FirebaseFirestore.getInstance();
+//        DocumentReference ref = fStore.collection("birds").document("bluejay");
+//        ref.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+//                System.out.println(documentSnapshot.getString("name"));
+//                System.out.println(documentSnapshot.getString("count"));
+//            }
+//        });
     }
-}
